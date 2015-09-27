a41778a3aae639eaa86104b2f3ba216b5cbc54cb
Bryan Guerre <bguerre12@gmail.com>
2015-09-26 21:54:00 -0700

    EMS Add Message Test functionality
    Added a temporary adding of a string

--- Roomies/app/src/main/java/com/ben/bryan/wilburn/roomies/EmsActivity.java
+++ Roomies/app/src/main/java/com/ben/bryan/wilburn/roomies/EmsActivity.java
@@ -1,14 +1,26 @@
 package com.ben.bryan.wilburn.roomies;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class EmsActivity extends Activity {
+
+    private String m_Text = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +31,14 @@ public class EmsActivity extends Activity {
         String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
 
         // Create the text view
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(message);
+        //TextView textView = new TextView(this);
+        //textView.setTextSize(40);
+        //textView.setText(message);
 
         // Set the text view as the activity layout
-        setContentView(textView);
+        //setContentView(textView);
+
+        setContentView(R.layout.activity_ems);
     }
 
     @Override
@@ -40,5 +54,53 @@ public class EmsActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void AddMessageClick(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add Message Input");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                m_Text = input.getText().toString();
+                AddTextView();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        //show the button
+        builder.show();
+    }
+
+    private void AddTextView() {
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.EmsLinearLayout);
+
+        Button button1 = new Button(this);
+
+        button1.setText(m_Text);
+        button1.setId(R.id.EmsLinearLayout);
+        //button1.setGravity(Gravity.LEFT);
+        button1.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        button1.setPadding(16,16,16,0);
+
+
+        Toast.makeText(this,m_Text, Toast.LENGTH_SHORT).show();
+
+        linearLayout.addView(button1);
     }
 }
