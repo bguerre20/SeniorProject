59d7ad57a10615f6bb3a4ebd2e7be937aefded10
Bryan Guerre <bguerre12@gmail.com>
2015-10-27 12:54:49 -0700

    trying to commit and push

---
+++ Roomies/app/src/main/java/com/ben/bryan/wilburn/roomies/ChoreActivity.java
@@ -0,0 +1,15 @@
+package com.ben.bryan.wilburn.roomies;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ChoreActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chore);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
