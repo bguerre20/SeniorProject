5de31ca3e0121b6d51fa7db9bc586657b8a6d92f
Enba417 <ben_carr_white@hotmail.com>
2015-10-29 15:46:08 -0700

    bulletin board pull from database

--- Roomies/app/src/main/java/com/ben/bryan/wilburn/roomies/BulletinBoardActivity.java
+++ Roomies/app/src/main/java/com/ben/bryan/wilburn/roomies/BulletinBoardActivity.java
@@ -3,6 +3,10 @@ package com.ben.bryan.wilburn.roomies;
 import android.os.Bundle;
 import android.app.Activity;
 
+import com.parse.*;
+
+import java.util.List;
+
 public class BulletinBoardActivity extends Activity {
 
     @Override
@@ -10,6 +14,18 @@ public class BulletinBoardActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bulletin_board);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+        ParseUser user = ParseUser.getCurrentUser();
+        ParseQuery<ParseObject> boardQuery = ParseQuery.getQuery("BulletinBoard");;
+        boardQuery.whereEqualTo("Apartment", user.get("Apartment"));
+        boardQuery.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e == null) {
+                    // The query was successful.
+                } else {
+                    // Something went wrong.
+                }
+            }
+        });
     }
 
 }
