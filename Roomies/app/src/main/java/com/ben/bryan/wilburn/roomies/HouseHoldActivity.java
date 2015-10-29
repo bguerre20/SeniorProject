ecc32a88d8c587bc926437c8b700e1e95ff3f0c1
Enba417 <ben_carr_white@hotmail.com>
2015-10-29 15:12:39 -0700

    adding some functionality to apartment

--- Roomies/app/src/main/java/com/ben/bryan/wilburn/roomies/HouseHoldActivity.java
+++ Roomies/app/src/main/java/com/ben/bryan/wilburn/roomies/HouseHoldActivity.java
@@ -2,6 +2,10 @@ package com.ben.bryan.wilburn.roomies;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import com.parse.*;
+
+import java.util.List;
 
 public class HouseHoldActivity extends Activity {
 
@@ -10,6 +14,19 @@ public class HouseHoldActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_house_hold);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    public void JoinClick (View view) {
+
+    }
+    public void CreateClick (View view) {
+
+    }
+    private boolean isInData (String name) throws ParseException {
+        boolean answer = false;
+        ParseQuery<ParseObject> apartmentQuery = ParseQuery.getQuery("Apartment");
+        apartmentQuery.whereEqualTo("ApartmentID", name);
+        List<ParseObject> check = apartmentQuery.find();
+        return !check.isEmpty();
     }
 
 }
