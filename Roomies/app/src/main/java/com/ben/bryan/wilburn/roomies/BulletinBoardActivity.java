2161cda95dd19f44f6a5c7f9a1f4fe7b2db29780
Bryan Guerre <bguerre12@gmail.com>
2015-10-29 16:15:36 -0700

    trying to get demo working of bulliten

--- Roomies/app/src/main/java/com/ben/bryan/wilburn/roomies/BulletinBoardActivity.java
+++ Roomies/app/src/main/java/com/ben/bryan/wilburn/roomies/BulletinBoardActivity.java
@@ -2,30 +2,58 @@ package com.ben.bryan.wilburn.roomies;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
 import com.parse.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BulletinBoardActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private ArrayList<String> listItems=new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    int clickCounter=0;
+
+
+    protected void onCreate(Bundle savedInstanceState, Bundle icicle) {
         super.onCreate(savedInstanceState);
+
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_bulletin_board);
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+
+
+
         setContentView(R.layout.activity_bulletin_board);
         getActionBar().setDisplayHomeAsUpEnabled(true);
         ParseUser user = ParseUser.getCurrentUser();
-        ParseQuery<ParseObject> boardQuery = ParseQuery.getQuery("BulletinBoard");;
+        final ParseQuery<ParseObject> boardQuery = ParseQuery.getQuery("BulletinBoard");
         boardQuery.whereEqualTo("Apartment", user.get("Apartment"));
         boardQuery.findInBackground(new FindCallback<ParseObject>() {
             public void done(List<ParseObject> objects, ParseException e) {
                 if (e == null) {
                     // The query was successful.
+                    for (int i = 0; i < objects.size(); i++) {
+                        listItems.add(objects.get(i).getString("Name"));
+                    }
                 } else {
                     // Something went wrong.
                 }
             }
         });
+
+
+    }
+
+
+    //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
+    public void addItems(View v) {
+        listItems.add("Clicked : " + clickCounter++);
+        adapter.notifyDataSetChanged();
     }
 
 }
