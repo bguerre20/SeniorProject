00b85b505fadc5bac507c6f830f9d1cfdb9e4f8e
Bryan Guerre <bguerre12@gmail.com>
2015-11-04 15:31:27 -0800

    presentation commit, sorry it's a bit late

--- Roomies/app/src/main/java/com/ben/bryan/wilburn/roomies/CreateBulletinActivity.java
+++ Roomies/app/src/main/java/com/ben/bryan/wilburn/roomies/CreateBulletinActivity.java
@@ -3,13 +3,14 @@ package com.ben.bryan.wilburn.roomies;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
+import android.widget.EditText;
 
 import com.parse.*;
 
 public class CreateBulletinActivity extends Activity {
 
     private String name;
-    private String discription;
+    private String description;
     private String date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +20,29 @@ public class CreateBulletinActivity extends Activity {
 
     }
     public void CreateClick (View view) throws ParseException {
-        name = "";
-        discription = "";
-        date = "";
+
+        EditText viewText = (EditText) findViewById(R.id.editText);
+        name = viewText.getText().toString();
+
+        viewText = (EditText) findViewById(R.id.editText2);
+        description = viewText.getText().toString();
+
+        //viewText = (EditText) findViewById(R.id.calendarView);
+        //date = viewText.getText().toString();
+
         ParseUser user = ParseUser.getCurrentUser();
-        ParseObject bulletin = new ParseObject("GameScore");
-        bulletin.put("DeleteDate", date);
-        bulletin.put("Notification", discription);
+        ParseObject bulletin = new ParseObject("BulletinBoard");
+        bulletin.put("DeleteDate", "Oct 31, 2015");
+        bulletin.put("Notification", description);
         bulletin.put("Name", name);
         bulletin.put("Apartment", user.get("Apartment"));
+
+        bulletin.saveInBackground();
+        finish();
     }
 
     public void CancelClick (View view) throws ParseException {
-
+        finish();
     }
 
 }
