e37e792f4abbab193b87faf6d87930e241f959a8
Wilburn Hou <wilburn.hou@gmail.com>
2015-12-08 14:14:56 -0800

    Login Test Update
    Added a few more test case for Login
    > Testing the location of the buttons

--- Roomies/app/src/test/java/com/ben/bryan/wilburn/roomies/LoginTest.java
+++ Roomies/app/src/test/java/com/ben/bryan/wilburn/roomies/LoginTest.java
@@ -1,31 +1,47 @@
 package com.ben.bryan.wilburn.roomies;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.ViewAsserts;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class LoginTest extends ActivityInstrumentationTestCase2<Login> {
     // Variables
     private Login mLogin;
     private EditText mUsername;
     private EditText mPassword;
+    private Button mLog;
+    private Button mRegister;
 
     @SuppressWarnings("deprecation")
     public LoginTest() {
         super("com.ben.bryan.wilburn.roomies.Login", Login.class);
     }
 
-    @Override
+    @Before
     protected void setUp() throws Exception {
         super.setUp();
         mLogin = this.getActivity();
     }
 
+    @SmallTest
     public void testPreconditions() {
         assertNotNull(mLogin);
         assertNotNull(mUsername);
         assertNotNull(mPassword);
+        assertNotNull(mLog);
+        assertNotNull(mRegister);
     }
 
+    @MediumTest
     public void testUsernameEdittext() {
         mUsername = (EditText) mLogin.findViewById(R.id.edittxt_username);
 
@@ -39,6 +55,7 @@ public class LoginTest extends ActivityInstrumentationTestCase2<Login> {
         assertEquals("test123", mUsername.getText());
     }
 
+    @MediumTest
     public void testPasswordEdittext() {
         mPassword = (EditText) mLogin.findViewById(R.id.edittxt_password);
 
@@ -50,5 +67,33 @@ public class LoginTest extends ActivityInstrumentationTestCase2<Login> {
         });
 
         assertEquals("test123", mPassword.getText());
+    }
+
+    @MediumTest
+    public void testLoginButton_layout() {
+        mLog = (Button) mLogin.findViewById(R.id.button_login);
+
+        final View decorView = mLogin.getWindow().getDecorView();
+
+        ViewAsserts.assertOnScreen(decorView, mLog);
+
+        final ViewGroup.LayoutParams layoutParams = mLog.getLayoutParams();
+        assertNotNull(layoutParams);
+        assertEquals(layoutParams.width, WindowManager.LayoutParams.MATCH_PARENT);
+        assertEquals(layoutParams.height, WindowManager.LayoutParams.WRAP_CONTENT);
+    }
+
+    @MediumTest
+    public void testRegisterButton_layout() {
+        mRegister = (Button) mLogin.findViewById(R.id.button_register);
+
+        final View decorView = mLogin.getWindow().getDecorView();
+
+        ViewAsserts.assertOnScreen(decorView, mRegister);
+
+        final ViewGroup.LayoutParams layoutParams = mRegister.getLayoutParams();
+        assertNotNull(layoutParams);
+        assertEquals(layoutParams.width, WindowManager.LayoutParams.MATCH_PARENT);
+        assertEquals(layoutParams.height, WindowManager.LayoutParams.WRAP_CONTENT);
     }
 }
